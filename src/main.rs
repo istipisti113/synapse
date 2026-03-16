63702f52e0a767b05b8c0443c9525ed8e6533fd7
Szabo Istvan <pisti4395@gmail.com>
2026-03-16 21:48:40 +0100

    mpris support added, async

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod ui;
 mod player;
 
-use std::{io, time::Duration};
+use std::{future::Future, io, thread, time::Duration};
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
@@ -9,8 +9,16 @@ use crossterm::{
 };
 use ratatui::{backend::CrosstermBackend, Terminal};
 use ui::app::App;
+use tokio;
+use tokio_util::sync::CancellationToken;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use mpris_server::{
+    zbus::{ Result}, Player,};
+
+use std::sync::{Arc, Mutex};
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let args: Vec<String> = std::env::args().collect();
     let music_path = args.get(1).cloned().unwrap_or_else(|| ".".to_string());
 
@@ -19,35 +27,89 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout))?;
 
-    let mut app = App::new(music_path);
-    
-    loop {
-        terminal.draw(|f| ui::render(f, &mut app))?;
-        app.update_time();
-        app.check_track_finished();
 
-        if event::poll(Duration::from_millis(100))? {
-            if let Event::Key(key) = event::read()? {
-                match key.code {
-                    KeyCode::Char('q') => break,
-                    KeyCode::Char('j') => app.next_song_in_list(),
-                    KeyCode::Char('k') => app.previous_song_in_list(),
-                    KeyCode::Char('h') => app.seek_backward(),
-                    KeyCode::Char('l') => app.seek_forward(),
-                    KeyCode::Char(' ') => app.toggle_playback(),
-                    KeyCode::Up => app.volume_up(),
-                    KeyCode::Down => app.volume_down(),
-                    KeyCode::Char('m') => app.toggle_play_mode(),
-                    KeyCode::Char('n') => app.next_track(),     // Переход на следующий трек
-                    KeyCode::Char('b') => app.previous_track(), // Переход на предыдущий трек
-                    KeyCode::Enter => app.play_selected(),
-                    _ => {}
+    let mpris_control = Arc::new(Mutex::new(String::from("")));
+
+    let token = CancellationToken::new();
+
+    //tokio::task::spawn_local(player.run());
+    let local = tokio::task::LocalSet::new();
+    let player = Player::builder("synapce")
+        .can_play(true)
+        .can_pause(true)
+        //.can_go_previous(true)
+        //.can_go_next(true)
+        .build()
+        .await.unwrap();
+
+    let ctrl = Arc::clone(&mpris_control);
+    player.connect_play_pause(move |_player| {
+        let mut control = ctrl.lock().unwrap();
+        control.push_str("playpause");
+    });
+
+    // Handle `Previous` method call
+    player.connect_previous(|_player| {
+        //println!("Previous");
+    });
+
+    // Handle `Next` method call
+    player.connect_next(|_player| {
+        //println!("Next");
+    });
+
+    // Handle `PlayPause` method call
+
+    let token_clone = token.clone();
+    tokio::task::spawn_blocking( move || {
+        let mut app = App::new(music_path);
+        loop {
+            terminal.draw(|f| ui::render(f, &mut app)).unwrap();
+            app.update_time();
+            app.check_track_finished();
+
+            let mut ctrl = mpris_control.lock().unwrap();
+            match &*ctrl.as_str() {
+                "playpause" =>{
+                    app.toggle_playback();
+                },
+                _ => {}
+            }
+            ctrl.clear();
+
+            if event::poll(Duration::from_millis(100)).unwrap() {
+                if let Event::Key(key) = event::read().unwrap() {
+                    match key.code {
+                        KeyCode::Char('q') => {
+                            token_clone.cancel();
+                            break;
+                        },
+                        KeyCode::Char('j') => app.next_song_in_list(),
+                        KeyCode::Char('k') => app.previous_song_in_list(),
+                        KeyCode::Char('h') => app.seek_backward(),
+                        KeyCode::Char('l') => app.seek_forward(),
+                        KeyCode::Char(' ') => app.toggle_playback(),
+                        KeyCode::Up => app.volume_up(),
+                        KeyCode::Down => app.volume_down(),
+                        KeyCode::Char('m') => app.toggle_play_mode(),
+                        KeyCode::Char('n') => app.next_track(),     // Переход на следующий трек
+                        KeyCode::Char('b') => app.previous_track(), // Переход на предыдущий трек
+                        KeyCode::Enter => app.play_selected(),
+                        _ => {}
+                    }
                 }
             }
         }
+
+
+        disable_raw_mode().unwrap();
+        execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture).unwrap();
+    });
+
+    tokio::select! {
+        _=player.run()=>{}
+        _=token.cancelled()=>{}
     }
 
-    disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture)?;
     Ok(())
 }
